7fd2ee5c0cde6011ab03ac146ec99f06ac22a512
DeVante Mullen <devantemullen@gmail.com>
2020-01-30 08:50:54 -0800

    enemy *not done

---
+++ src/AppWindow.java
@@ -0,0 +1,58 @@
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+public class AppWindow extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.setTitle("Zombie Survival");
+        Button btn = new Button("Start Game");
+        btn.setOnAction(e -> {
+            System.out.println("Starting...");
+            primaryStage.setScene(createMap(true));
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, window_width, window_height));
+        //primaryStage.setScene(new Scene(createMap()));
+        primaryStage.show();
+    }
+
+    private static int window_width = 600;
+    private static int window_height = 400;
+
+    private Scene createMap(){
+        Pane root = new Pane();
+        root.setPrefSize(window_width, window_height);
+        Map map = new Map();
+        root.getChildren().add(map.getPanes());
+        Scene newScene = new Scene(root);
+        return newScene;
+    }
+    private Scene createMap(boolean hasPlayer){
+        Pane root = new Pane();
+        root.setPrefSize(window_width, window_height);
+        Map map = new Map();
+        root.getChildren().add(map.getPanes());
+        Scene newScene = new Scene(root);
+        Player player;
+        if(hasPlayer)
+            player = new Player(newScene, root, map);
+        return newScene;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
+
